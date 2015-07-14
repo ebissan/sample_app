f993f62aae1eb671c8ef3134206607ab9383cb23
ebis <sabaoheshiko@gmail.com>
2015-07-14 17:14:36 +0900

    home help about edit title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,6 +10,36 @@
 #  end
 #end
 
+#require 'spec_helper'
+#
+#describe "Static pages" do
+#
+#  describe "Home page" do
+#
+#    it "should have the content 'Sample App'" do
+#      visit '/static_pages/home'
+#      expect(page).to have_content('Sample App')
+#    end
+#  end
+#
+#  describe "Help page" do
+#
+#    it "should have the content 'Help'" do
+#      visit '/static_pages/help'
+#      expect(page).to have_content('Help')
+#    end
+#  end
+#
+#  describe "About page" do
+#
+#    it "should have the content 'About Us'" do
+#      visit '/static_pages/about'
+#      expect(page).to have_content('About Us')
+#    end
+#  end
+#
+#end
+
 require 'spec_helper'
 
 describe "Static pages" do
@@ -20,6 +50,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
   describe "Help page" do
@@ -27,6 +62,11 @@ describe "Static pages" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
@@ -36,7 +76,11 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
-  end
 
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
 end
 
